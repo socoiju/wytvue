77d18076927c409ba8d89f0ada221df84fae183f
socoi.ks <socoi.ju+ks@gmail.com>
2023-04-11 15:32:34 +0900

    增加el表格和card

--- src/components/vue0410Pinia.vue
+++ src/components/vue0410Pinia.vue
@@ -5,12 +5,34 @@
   <el-button class="button1" @click="zj">增加{{n}}</el-button>
   <el-button class="button1" @click="js">减少{{n}}</el-button>
   <el-button class="button1" @click="axiosxx">axios请求</el-button><!-- 写了-stop，还是会触发按钮的click-->
-  <p>获得第{{dj}}个数据(0-100)</p><h4>{{ti}}{{id}}</h4><p>{{dd}}</p>
-  <el-button @click="kx">——</el-button>
-  <p>获得第{{dj}}个defaultData数据(0-10)</p><h4>{{counter.moneyData[counter.count]}}</h4>
-  <el-button  class="button1" round="8px" color="#626aef" :dark="isDark" type="primary">Primary</el-button>
+  <div style="display:flex;justify-content: space-between;margin-top:10px">
+      <el-card class="box-card" >
+            <div class="card-header">
+                <span>axios 获得第{{dj}}个数据(0-100)</span>
+            </div>
+        <div  class="text item">{{ti}}{{id}}<br>{{dd}}</div>
+    </el-card>
+    <el-card class="box-card" style="margin-left:10px">
+        <div class="card-header">
+            <span>pinia 获得第{{dj}}个defaultData数据(0-10)</span>
+        </div>
+        <div  class="text item">{{counter.moneyData[counter.count]}}</div>
+    </el-card>
+  </div>
+  <el-button class="button1" @click="kx" style="margin-top:10px">——</el-button>
+  <p></p>
+  <el-button  class="button1" round="8px" color="#626aef">Primary</el-button>
   <el-button  class="button1"  type="success">Success</el-button>
   <el-button  type="success"> Success</el-button>
+  <el-table :data="tableData" border style="width: 100%"  class="moneyTable">
+      <el-table-column prop="dt" label="日付" width="180"/>
+      <el-table-column prop="nm" label="相手勘定" width="180"/>
+      <el-table-column prop="zy" label="摘要"/>
+      <el-table-column prop="hq" label="出金"/>
+      <el-table-column prop="dq" label="入金"/>
+  </el-table>
+  <br><br><br><br>
+
 </template>
 
 <script setup>
@@ -23,7 +45,7 @@ const counter = useCounterStore();
 let dj=ref(counter.count);
 let [id,ti,dd,n]= [ref(null),ref(''),ref(''),ref(1)];
 //let data=[...counter.moneyData];
-
+let tableData=[...counter.moneyData];
 //data=[...counter.moneyData[counter.count]];
 // 这句的失败原因是右边的第0个是花括号的对象而不是数组！
 
@@ -41,15 +63,18 @@ function axiosxx(){//获取网上的json
 
 function kx(){
     if(counter.count<=counter.moneyData.length){
-        //data=[...counter.moneyData];
+        tableData=[...counter.moneyData];
         console.log(`处理完毕${counter.moneyData[counter.count]}`);
     }else{
         console.log("没有这个值啦");
     }
+
+
 }
 onBeforeMount(() => {
     counter.tableData()
     axiosxx()
+    kx()
 })
 function zj(){
     console.log('增加增加！');
@@ -67,10 +92,9 @@ function js(){
 </script>
 
 <style >
-h4,p{font-size:15px;}
+h4,p{font-size:14px;}
 input:focus{
-    border-radius:0px!important;
-    border-width:1px!important;
+    outline: none;
 }
 input{
     background: white;
@@ -106,6 +130,33 @@ input{
     width-min:60px;height:28px;
     margin-left: 10px;
 }
+.moneyTable{
+    margin-top: 10px;
+    color:black!important;
+}
+.card-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
 
+.text {
+    font-size: 14px;
+    justify-content: left;
+    text-align:left;
+}
 
+.item {
+    margin-bottom: 18px;
+}
+
+.box-card {
+    background: white!important;
+    border-color:black!important;
+    border-radius:0px!important;
+    border-width:1px!important;
+    height-max: 200px!important;
+    width-max: 200px!important;
+    box-shadow:none!important;
+}
 </style>
